cebde7083f0cf53c3e4d65150cdda38f333f5248
jerry12122 <a0975844268@gmail.com>
2022-09-24 21:46:23 +0800

    更新排程設置

--- entry.sh
+++ entry.sh
@@ -3,7 +3,10 @@ curl -sSL https://get.acme.sh | sh -s email=$ACME_EMAIL
 ln -s /root/.acme.sh/acme.sh /usr/bin/acme.sh
 acme.sh --set-default-ca --server letsencrypt
 crontab -l > mycron
-echo "0 0 * * * acme.sh  --home /acme --server letsencrypt --issue  --dns dns_cf  $DOMAIN --force >> /log/acme.log" >> mycron
+for i in $(echo $DOMAIN | tr ";" "\n")
+do
+    echo "0 0 * * * acme.sh  --home /acme --server letsencrypt --issue  --dns dns_cf  $DOMAIN --force > /log/acme.log" >> mycron
+done
 crontab mycron
 rm mycron
 mkdir -p /log
