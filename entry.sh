546a23c00c778315d3f8e1673117629695b81663
jerry12122 <a0975844268@gmail.com>
2022-09-24 22:14:12 +0800

    crontab bug fix

--- entry.sh
+++ entry.sh
@@ -2,12 +2,14 @@
 curl -sSL https://get.acme.sh | sh -s email=$ACME_EMAIL
 ln -s /root/.acme.sh/acme.sh /usr/bin/acme.sh
 acme.sh --set-default-ca --server letsencrypt
-crontab -l > mycron
-for i in $(echo $DOMAIN | tr ";" "\n")
-do
-    echo "0 0 * * * acme.sh  --home /acme --server letsencrypt --issue  --dns dns_cf  $i --force >> /log/acme.log" >> mycron
-done
 crontab -r
+crontab -l > mycron
+IFS=';'
+read -ra arr <<< "$DOMAIN"
+for val in "${arr[@]}";
+do
+  echo "0 0 * * * acme.sh  --home /acme --server letsencrypt --issue  --dns dns_cf  $val --force >> /log/acme.log" >> mycron
+done
 crontab mycron
 rm mycron
 mkdir -p /log
